c558bdac5ed87514815869ca751dfd82d2de6213
catherine-tranchand <catherine.tranchand@laplateforme.io>
2024-06-25 13:43:49 +0200

    :fire: created shopController and displayed products

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ShopController extends AbstractController
+{
+    #[Route('/shop', name: 'app_shop')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+        return $this->render('shop/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+}
