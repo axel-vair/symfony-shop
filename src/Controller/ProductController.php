e3b37a2b6f26c3478bcf45468802d77f18bd0ad0
Axel Vair <axel@Macbook.local>
2024-06-26 14:18:53 +0200

    add footer & show product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/product/{id}', name: 'app_product_id', methods: ['GET'])]
+    public function index(Product $product, ProductRepository $productRepository): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+        return $this->render('product/show.html.twig', [
+            "product" => $product,
         ]);
     }
 }
