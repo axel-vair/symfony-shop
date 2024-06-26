cf0aa0f69cc1aa3e16f83d19ccdd25426bf7a3e1
Axel Vair <axel@Macbook.local>
2024-06-26 16:59:00 +0200

    :tada: add pagination & turbo frame on shop

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -3,18 +3,31 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use Pagerfanta\Adapter\ArrayAdapter;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'app_shop')]
-    public function index(ProductRepository $productRepository): Response
-    {
-        $products = $productRepository->findAll();
+    public function index(
+        ProductRepository $productRepository,
+        #[MapQueryParameter] int $page = 1
+    ): Response {
+        $queryBuilder = $productRepository->createQueryBuilder('p');
+
+        $adapter = new QueryAdapter($queryBuilder);
+        $pager = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new ArrayAdapter($productRepository->findAll()),
+            $page,
+            6);
+
         return $this->render('pages/shop/index.html.twig', [
-            'products' => $products,
+            'products' => $pager,
         ]);
     }
 }
