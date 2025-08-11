a64211fad4b01ae6b0ca040a48cb23f215c8d38b
Axel Vair <dev@local.test>
2025-08-11 11:24:43 +0200

    Add lastName and firstName on UserType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,8 +7,11 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
@@ -22,17 +25,66 @@ class UserType extends AbstractType
                 ],
                 'label_attr' => ['class' => 'form-label inline-block mb-2 text-gray-700'],
             ])
-            ->add('password', PasswordType::class, [
-                'mapped' => false,
-                'label' => 'Mot de passe actuel',
-                'required' => true,
+            ->add('firstName', TextType::class, [
                 'attr' => [
-                    'class' => 'form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none',
-                    'placeholder' => 'Votre mot de passe actuel'
+                    'class' => 'mb-3 form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none',
+                    'placeholder' => 'Prénom'
                 ],
+                'label' => 'Prénom',
                 'label_attr' => ['class' => 'form-label inline-block mb-2 text-gray-700'],
+                'constraints' => [
+                    new Length([
+                        'min' => 1,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 30,
+                        'maxMessage' => 'Votre mot de passe doit contenir moins de {{ limit }} caractères',
+
+                    ])
+                ],
+                'required' => false,
             ])
-            ->add('Modifier', SubmitType::class, [
+            ->add('lastName', TextType::class, [
+                'attr' => [
+                    'class' => 'mb-3 form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none',
+                    'placeholder' => 'Nom'
+                ],
+                'label' => 'Nom',
+                'label_attr' => ['class' => 'form-label inline-block mb-2 text-gray-700'],
+                'constraints' => [
+                    new Length([
+                        'min' => 1,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 30,
+                        'maxMessage' => 'Votre mot de passe doit contenir moins de {{ limit }} caractères',
+
+                    ])
+                ],
+                'required' => false,
+            ])
+
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none',
+                    'placeholder' => 'Mot de passe'
+                ],
+                'label' => 'Mot de passe*',
+                'label_attr' => ['class' => 'form-label inline-block mb-2 text-gray-700'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                ],
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire',
                 'attr' => ['class' => 'd-grid btn btn-dark mt-4 w-100'],
             ])
         ;
